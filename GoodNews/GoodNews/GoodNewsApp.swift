5b51c8e87dc3d60ee481ac1cfc98c3aa385ae937
Bianca Maciel <macielmatosb@gmail.com>
2025-09-18 08:27:35 -0300

    Adding XCode project

---
+++ GoodNews/GoodNews/GoodNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodNewsApp.swift
+//  GoodNews
+//
+//  Created by Bianca Maciel on 18/09/25.
+//
+
+import SwiftUI
+
+@main
+struct GoodNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
