a484d8a4d61e481c1f98b9e897c8c9c3d5105b3d
Bianca Maciel <macielmatosb@gmail.com>
2025-10-13 12:46:22 -0300

    Creating a Service specific to categories

--- GoodNews/GoodNews/Modules/Networking/Services/CategoryService.swift
+++ GoodNews/GoodNews/Modules/Networking/Services/CategoryService.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+protocol CategoryServiceProtocol {
+    func getAllHeadlines(completion: @escaping ([Category]) -> ())
+}
+
+
+final class CategoryService: CategoryServiceProtocol {
+    
+    let repository: NewsRepositoryProtocol
+    
+    init(repository: NewsRepositoryProtocol = NewsRepository()) {
+        self.repository = repository
+    }
+    
+    func getAllHeadlines(completion: @escaping ([Category]) -> ()) {
+        
+        var categories = [Category]()
+        var requestCount = 0
+        let categoriesCount = Category.all().count
+        
+        Category.all().forEach { category in
+            repository.fetchNews(Article.by(category)) { articles in
+                
+                requestCount += 1
+                
+                guard let articles = articles else { return }
+                
+                let category = Category(title: category, articles: articles)
+                categories.append(category)
+                
+                if requestCount == categoriesCount {
+                    DispatchQueue.main.async {
+                        completion(categories)
+                    }
+                }
+            }
+        }
+    }
+}
