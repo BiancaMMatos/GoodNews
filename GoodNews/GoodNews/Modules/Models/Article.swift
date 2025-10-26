8be9a374fecf13aa2339573a0d227f39a8711dbc
Bianca Maciel <macielmatosb@gmail.com>
2025-10-26 18:38:01 -0300

    Correcting value from imageURL on mocked articles

--- GoodNews/GoodNews/Modules/Models/Article.swift
+++ GoodNews/GoodNews/Modules/Models/Article.swift
@@ -38,12 +38,12 @@ extension Article {
 
 extension Article {
     static let mockedArticles: [Article] = [
-        Article(title: "News 1", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", url: nil, imageURL: "example"),
+        Article(title: "News 1", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", url: nil, imageURL: "mockedImage"),
         
-        Article(title: "News 2", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", url: nil, imageURL: "example"),
+        Article(title: "News 2", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", url: nil, imageURL: "mockedImage"),
         
-        Article(title: "News 3", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", url: nil, imageURL: "example"),
+        Article(title: "News 3", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", url: nil, imageURL: "mockedImage"),
         
-        Article(title: "News 4", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", url: nil, imageURL: "example")
+        Article(title: "News 4", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", url: nil, imageURL: "mockedImage")
     ]
 }
